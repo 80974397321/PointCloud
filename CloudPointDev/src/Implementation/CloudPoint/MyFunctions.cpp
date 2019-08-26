5a7d8fdcd47a97c484803683663fa9356be5af14
80974397321 <32105251+80974397321@users.noreply.github.com>
2019-08-26 23:53:53 +0300

    Initial commit for dev

---
+++ CloudPointDev/src/Implementation/CloudPoint/MyFunctions.cpp
@@ -0,0 +1,17 @@
+/*
+ * MyFunctions.cpp
+ *
+ *  Created on: Aug 20, 2019
+ *      Author: u1
+ */
+
+#include "CloudPoint/MyFunctions.h"
+
+MyFunctions::MyFunctions() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MyFunctions::~MyFunctions() {
+	// TODO Auto-generated destructor stub
+}
