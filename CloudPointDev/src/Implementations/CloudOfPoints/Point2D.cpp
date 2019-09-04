623e33fde4c8748e9998f86aeb2caad41a2a5699
Dennis <32113099+ElKoriano@users.noreply.github.com>
2019-09-04 21:31:51 +0300

    Add files via upload

--- CloudPointDev/src/Implementations/CloudOfPoints/Point2D.cpp
+++ CloudPointDev/src/Implementations/CloudOfPoints/Point2D.cpp
@@ -5,11 +5,12 @@
  *      Author: u1
  */
 
-#include "CloudOfPoints/Point2D.h"
+#include "Point2D.h"
 
 Point2D::Point2D() {
 	// TODO Auto-generated constructor stub
-
+	p[POINT2D_X]=0.0;
+	p[POINT2D_Y]=0.0;
 }
 
 Point2D::~Point2D() {
